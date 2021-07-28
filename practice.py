c2e2f73a521ddc14732034d7c167da9e4b30f7bb
DavidKimDY <kdaeyoub@gmail.com>
2021-07-28 21:33:24 +0900

    with cookie

--- practice.py
+++ practice.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask import request
+from flask import make_response
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    response = make_response('<h1>This document carries a cookie </h1>')
+    response.set_cookie('answer', '42')
+    return response
 
+
+@app.route('/user/<name>')
+def user(name):
+    return f'<h1>Hello {name}!</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
