42a8ee8f982e03d0434b1ff6ea8e53514cd12ddf
DavidKimDY <kdaeyoub@gmail.com>
2021-07-31 15:26:01 +0900

    modified

--- practice.py
+++ practice.py
@@ -1,20 +1,18 @@
 from flask import Flask
 from flask import request
 from flask import make_response
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    response = make_response('<h1>This document carries a cookie </h1>')
-    response.set_cookie('answer', '42')
-    return response
-
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return f'<h1>Hello {name}!</h1>'
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
